ca5b3e85ed56dacd45d83b1e18c34cca6f3addb8
gfmota <gabrielfmota134@gmail.com>
2024-05-26 13:57:48 -0300

    create data analyzer
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- src/main/java/com/template/template/ChargingPlugDataAnalyzerApplication.java
+++ src/main/java/com/template/template/ChargingPlugDataAnalyzerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class ChargingPlugDataAnalyzerApplication {
   public static void main(String[] args) {
-    SpringApplication.run(TemplateApplication.class, args);
+    SpringApplication.run(ChargingPlugDataAnalyzerApplication.class, args);
   }
 }
